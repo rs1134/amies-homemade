a44623ff8285188227bb0f784e44fe6b4bfbb097
Rushil <rushil@Rushils-MacBook-Air.local>
2026-07-11 12:44:29 +0530

    Replace 3rd gallery image for Homemade Healthy Granola

--- src/constants.ts
+++ src/constants.ts
@@ -680,7 +680,7 @@ export const PRODUCTS: Product[] = [
     images: [
       'https://ik.imagekit.io/amieshomemade/Untitled%20design%20(43).jpg',
       'https://ik.imagekit.io/amieshomemade/Untitled%20design%20(44).jpg',
-      'https://ik.imagekit.io/amieshomemade/067A3625%20(1).JPG',
+      'https://ik.imagekit.io/amieshomemade/ChatGPT%20Image%20Jul%204,%202026,%2005_59_49%20PM.png?updatedAt=1783168320890',
       'https://ik.imagekit.io/amieshomemade/ChatGPT%20Image%20Jun%2023,%202026,%2011_38_47%20AM.png?updatedAt=1782444349716',
       'https://ik.imagekit.io/amieshomemade/Screenshot%202026-06-26%20at%203.01.54%E2%80%AFPM.png',
     ],
