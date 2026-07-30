63aa66401f310e20f66776ada38290a0e09cec4b
Rushil <rushil@Rushils-MacBook-Air.local>
2026-07-30 20:31:53 +0530

    Replace 2nd image in Digestive Crunch

--- src/constants.ts
+++ src/constants.ts
@@ -300,7 +300,7 @@ export const PRODUCTS: Product[] = [
     image: 'https://ik.imagekit.io/amieshomemade/Untitled%20design%20(20)_9Vt-dPhkV.jpg',
     images: [
       'https://ik.imagekit.io/amieshomemade/Untitled%20design%20(20)_9Vt-dPhkV.jpg',
-      'https://ik.imagekit.io/amieshomemade/Untitled%20design%20(18)_Zbgo2JRO1y.jpg',
+      'https://ik.imagekit.io/amieshomemade/067A3474.JPG',
       'https://ik.imagekit.io/amieshomemade/Untitled%20design%20(19)_cz9NFk147.jpg',
       'https://ik.imagekit.io/amieshomemade/ChatGPT%20Image%20Jun%2023,%202026,%2011_24_07%20AM.png?updatedAt=1782444349689',
       'https://ik.imagekit.io/amieshomemade/ChatGPT%20Image%20Jun%2028,%202026,%2009_58_40%20AM.png',
