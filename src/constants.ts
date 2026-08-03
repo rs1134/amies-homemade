54dddfd3bb7dc4ce3c2ed8810c61901e552602cf
Rushil <rushil@Rushils-MacBook-Air.local>
2026-08-03 22:17:09 +0530

    Add 3rd image to Gourmet Discovery hamper

--- src/constants.ts
+++ src/constants.ts
@@ -52,6 +52,7 @@ export const PRODUCTS: Product[] = [
     images: [
       'https://ik.imagekit.io/amieshomemade/Untitled%20design%20(14).png',
       'https://ik.imagekit.io/amieshomemade/Untitled%20design%20(50).jpg',
+      'https://ik.imagekit.io/amieshomemade/Untitled%20design%20(14).png?updatedAt=1785557156908',
     ],
     ingredients: ['Dryfruit & Seeds (200g)', 'Homemade Granola (250g)', 'Almond Motichoor Ladoo (250g)'],
     isGift: true,
